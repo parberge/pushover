989932254c03c65d03f65f3d388ffa5ae6f81b13
peerster <paer.berge@gmail.com>
2016-08-08 22:19:21 +0200

    Add a test for pushover token

--- test_pushover.py
+++ test_pushover.py
@@ -10,3 +10,9 @@ def pushover():
 def test_pushover_exception(pushover):
     with pytest.raises(pushover.PushoverError):
         pushover.Pushover()
+
+def test_pushover_token(pushover):
+    # According to https://pushover.net/api the token should be 30 chars long
+    my_test_token = "A" * 30
+    pushover_obj = pushover.Pushover(my_test_token)
+    assert(len(pushover_obj.token) == 30)
