001625801b9d00455d6ae5c04a3bf5d5bd1f89e6
peerster <paer.berge@gmail.com>
2016-08-09 12:47:32 +0200

    Rewrite the test setup
    Also added a test for user token

--- test_pushover.py
+++ test_pushover.py
@@ -1,18 +1,36 @@
 #!/usr/bin/env python3
-
 import pytest
+import re
+
+# The app and user token must only contain valid chars: I.e [a-zA-Z0-9_]
+# and be 30 chars in length
+pushover_token_pattern = re.compile('^\w{30}$')
 
 @pytest.fixture
 def pushover():
     import pushover
     return pushover
 
+@pytest.fixture
+def pushover_obj(pushover):
+    """
+    Sets up the pushover obj for testing
+    """
+
+    test_app_token = "A" * 30
+    test_user_token = "B" * 30
+    pushover_obj = pushover.Pushover(test_app_token)
+    pushover_obj.user(test_user_token)
+
+    return pushover_obj
+
 def test_pushover_exception(pushover):
     with pytest.raises(pushover.PushoverError):
+        # Should raise exception about no app token supplied
         pushover.Pushover()
 
-def test_pushover_token(pushover):
-    # According to https://pushover.net/api the token should be 30 chars long
-    my_test_token = "A" * 30
-    pushover_obj = pushover.Pushover(my_test_token)
-    assert(len(pushover_obj.token) == 30)
+def test_pushover_app_token(pushover_obj):
+    assert(pushover_token_pattern.match(pushover_obj.token))
+
+def test_pushover_user_token(pushover_obj):
+    assert(pushover_token_pattern.match(pushover_obj.user_token))
